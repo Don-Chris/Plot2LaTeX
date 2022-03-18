c0d91469282846e41391cf42de3eb46116c845ec
Don-Chris <38617646+Don-Chris@users.noreply.github.com>
2022-03-18 10:03:56 +0100

    Undo add ChangeInterpreter to Plot2LaTeX

--- Plot2LaTeX.m
+++ Plot2LaTeX.m
@@ -630,27 +630,3 @@ else
     warning([' - check_Inkscape_Version.m: system(''',inkscape_path,' --version'') was not successful. System response was ',num2str(status),'.'])
 end
 end
-
-%% ------------------------------------------------------------------------
-function ChangeInterpreter(h,Interpreter)
-% CHANGEINTERPRETER puts interpeters in figure h to Interpreter
-
-    TexObj = findall(h,'Type','Text');
-    LegObj = findall(h,'Type','Legend');
-    AxeObj = findall(h,'Type','Axes');  
-    ColObj = findall(h,'Type','Colorbar');
-    
-    Obj = [TexObj;LegObj]; % Tex and Legend opbjects can be treated similar
-    
-    n_Obj = length(Obj);
-    for i = 1:n_Obj
-        Obj(i).Interpreter = Interpreter;
-    end
-    
-    Obj = [AxeObj;ColObj]; % Axes and colorbar opbjects can be treated similar
-    
-    n_Obj = length(Obj);
-    for i = 1:n_Obj
-        Obj(i).TickLabelInterpreter = Interpreter;
-    end
-end
